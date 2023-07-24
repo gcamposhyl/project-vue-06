2ea708fbd6c08925be976ab31255b793a7ffdfc7
gcamposhyl <gcampos@hyl.cl>
2023-07-23 20:46:15 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Notas
+
+- Tailwind
+    - npm install -D tailwindcss postcss autoprefixer
+    - npx tailwindcss init -p
---
+++ src/views/ClientesView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-6xl">Clientes</h1>
+
+    </div>
+    
+</template>
